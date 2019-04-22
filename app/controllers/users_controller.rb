1c2373bb47d185663c91210d7096a403547e9e77
Mohamed Ghorab <mody.moustafa@yahoo.com>
2019-04-22 16:08:32 +0200

    Auth Links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
     def index
         @users = User.page(params[:page]).per(5)
     end
     
     def show
-        @user = User.find(params[:id])
         @user_articles = @user.articles.page(params[:page])
         # @user.articles.page(1).total_pages = 5
         # @user_articles = Kaminari.paginate_array(@user.articles)
@@ -25,11 +25,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "Your account has been updated successfully"
             redirect_to articles_path
@@ -41,5 +39,8 @@ class UsersController < ApplicationController
     private
         def user_params
             params.require(:user).permit(:username, :email, :password)
+        end
+        def set_user
+            @user = User.find(params[:id])
         end
 end
